d00ce2d092daaa5c51a5528a85d21c76e073f435
Venkatareddie <84559316+Venkatareddie@users.noreply.github.com>
2024-11-22 16:28:29 -0600

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,12 +1,5 @@
 Class Hello{
-}[D}[D
-
-
-[A[A[A
-
-[A[A
-
-[A[A[3~[public static void main(String args[])
+public static void main(String args[])
 {
 System.out.println("Hello World");
 }
